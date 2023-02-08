892f63ec008272b091c569e9d7ea9738deabb182
R <90389727+notebrr@users.noreply.github.com>
2023-02-08 09:58:47 +0100

    d

--- npmPackage/src/exercise.js
+++ npmPackage/src/exercise.js
@@ -6,14 +6,30 @@ let data;
 fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) =>{
     superagent
         .get(`https://dog.ceo/api/breed/${data.trim().toLowerCase()}/images/random`)
-        .end((end,res) => {
-            if(err) return console.log(err)
+        .then(res => {
             console.log(res.body.message)
-            data = res.body.message
-
             fs.writeFile("test.txt", res.body.message, (err) => {
-                if(err) return console.log(err);
-                console.log("Dog image saved to file")
+                if(err) return console.log(err)
+                console.log("Image saved successfully")
             })
         })
+        .catch(err => console.log("Error", err));
 })
+
+const readFilePro = (file) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, "utf-8", (err, data) => {
+            if(err) reject(err);
+            resolve(data);
+        })
+    })
+}
+
+const writeFilePro = (file, myString) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, myString, (err, data) => {
+            if(err) reject(err);
+            resolve("");
+        })
+    })
+}
